1b8ed4f0aec510a7407804b0f5e96339ce30e8b7
nathanielzheng <nathanielzheng@gmail.com>
2019-01-20 17:26:35 -0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,5 +5,5 @@ with graph2.as_default():
     Scalar = tf.constant(2)
     Vector = tf.constant([5,6,2])
     Matrix = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
-    Tensor = tf.constant( [ [[1,2,3],[2,3,4],[3,4,5]] , [[4,5,6],[5,6,7],[6,7,8]] , [[7,8,9],[8,9,10],[9,10,11]] ] )
+
 
