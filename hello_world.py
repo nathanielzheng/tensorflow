e7a688b232bd2246693fecd53ce0c577c29b06de
nathanielzheng <nathanielzheng@gmail.com>
2019-01-20 17:25:00 -0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -6,4 +6,4 @@ with graph2.as_default():
     Vector = tf.constant([5,6,2])
     Matrix = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
     Tensor = tf.constant( [ [[1,2,3],[2,3,4],[3,4,5]] , [[4,5,6],[5,6,7],[6,7,8]] , [[7,8,9],[8,9,10],[9,10,11]] ] )
-with tf.Session(graph = graph2) as sess:
+
